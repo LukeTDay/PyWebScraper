0121d7cc78c13fbf1352ea86d4e67e94e5b53843
Permabannedrotmg <luke.day@gmail.com>
2025-10-10 10:55:40 -0400

    Created HTML for website that shows a table of the JSON

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
-def Home():
-    return "Hello World!"
+def home():
+    try:
+        with open("jobs.json", "r", encoding="utf-8") as f:
+            jobs = json.load(f)
+    except FileNotFoundError:
+        jobs = []
+    return render_template("index.html", jobs=jobs)
 
 if __name__ == "__main__":
     app.run(debug=True)
