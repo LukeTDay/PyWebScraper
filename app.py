286f266c9831de0266e006d19ff56e7eef07fa67
Permabannedrotmg <luke.day@gmail.com>
2025-10-10 12:28:34 -0400

    Added pages to the table so that it cannot be overloaded as well as a pagefor interacting with the raw json

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import json
 
+JOBS_PER_PAGE = 10
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    page = int(request.args.get("page", 1))
     try:
         with open("jobs.json", "r", encoding="utf-8") as f:
             jobs = json.load(f)
+
+            jobs = sorted(jobs, key=lambda j: j['job_post_time'], reverse=True)
+            start = (page - 1) * JOBS_PER_PAGE
+            end =  start + JOBS_PER_PAGE
+            jobs_page = jobs[start:end]
+            total_pages = (len(jobs) + JOBS_PER_PAGE - 1) // JOBS_PER_PAGE
+
+
     except FileNotFoundError:
         jobs = []
-    return render_template("index.html", jobs=jobs)
+    return render_template(
+        "index.html", 
+        jobs=jobs_page,
+        page = page,
+        total_pages = total_pages)
+
+@app.route("/rawjson")
+def deliverJSON():
+    with open("jobs.json", "r", encoding = "utf-8") as f:
+        jobs = json.load(f)
+    return jsonify(jobs)
 
 if __name__ == "__main__":
     app.run(debug=True, extra_files=["templates/index.html"])
